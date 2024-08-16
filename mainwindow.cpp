a8c0f05969118ca9432bd9007a4f60a425059c38
Jaime Redondo <jairegra@gmail.com>
2024-08-16 12:37:54 +0200

    Implement baseline graphing

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,16 @@
 #include <QtSerialPort/QSerialPortInfo>
 #include <QtSerialPort/QSerialPort>
 
+#include <QtCharts>
+
 #include <QFileDialog>
 #include <QMessageBox>
 #include <QScrollBar>
 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+
+/* ========== CONSTRUCTOR/DESTRUCTOR ========= */
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -26,12 +30,30 @@ MainWindow::MainWindow(QWidget *parent)
     connect(port, SIGNAL(readyRead()), this, SLOT(serial_read_data()));
 
     distribute_table_width();
+
+    cpm_series = new QLineSeries();
+    avg_cpm_series = new QScatterSeries();
+    usv_series = new QLineSeries();
+    avg_usv_series = new QScatterSeries();
+    chart = new QChart();
+    axis_time = new QValueAxis();
+    axis_cpm = new QValueAxis();
+    axis_usv = new QValueAxis();
+
+    yellow = QColor(0xfa, 0xbd, 0x2f);
+    red = QColor(0xfb, 0x49, 0x34);
+    blue = QColor(0x45, 0x85, 0x88);
+    green = QColor(0x98, 0x97, 0x1a);
+
+    graph_setup();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+/* ========== METHODS ========= */
 
 void MainWindow::get_ports()
 {
@@ -110,9 +132,13 @@ void MainWindow::serial_read_data()
 
     if (done_reading) {
         ui->text_output->insertPlainText(serial_data);
+        ui->text_output->moveCursor(QTextCursor::End);
         ui->button_save->setEnabled(true);
 
-        if (output_valid_format()) output_add_row();
+        if (output_valid_format()) {
+            output_add_row();
+            graph_plot();
+        }
 
         serial_data = "";
         done_reading = false;
@@ -123,6 +149,103 @@ void MainWindow::distribute_table_width()
 {
     ui->table_output->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
 }
+
+void MainWindow::graph_setup()
+{
+    chart->legend()->hide();
+    chart->setTitle("Radiation Data");
+    QFont chart_font;
+    chart_font.setBold(true);
+    chart->setTitleFont(chart_font);
+
+    axis_time->setTitleText("Time (s)");
+    QFont axis_font;
+    axis_font.setBold(false);
+    axis_time->setTitleFont(axis_font);
+    chart->addAxis(axis_time, Qt::AlignBottom);
+
+    chart->addSeries(cpm_series);
+    chart->addSeries(avg_cpm_series);
+
+    axis_cpm->setTitleText("Count rate (CPM)");
+    axis_cpm->setTitleFont(axis_font);
+    chart->addAxis(axis_cpm, Qt::AlignLeft);
+
+    cpm_series->setPointsVisible(true);
+    cpm_series->setColor(yellow);
+    cpm_series->attachAxis(axis_time);
+    cpm_series->attachAxis(axis_cpm);
+
+    avg_cpm_series->setMarkerSize(8);
+    avg_cpm_series->setMarkerShape(QScatterSeries::MarkerShapeRectangle);
+    avg_cpm_series->setColor(red);
+    avg_cpm_series->attachAxis(axis_time);
+    avg_cpm_series->attachAxis(axis_cpm);
+
+    chart->addSeries(usv_series);
+    chart->addSeries(avg_usv_series);
+
+    axis_usv->setTitleText("Dose rate (µSv/h)");
+    axis_usv->setTitleFont(axis_font);
+    chart->addAxis(axis_usv, Qt::AlignRight);
+
+    usv_series->setPointsVisible(true);
+    usv_series->setColor(blue);
+    usv_series->attachAxis(axis_time);
+    usv_series->attachAxis(axis_usv);
+
+    avg_usv_series->setMarkerSize(8);
+    avg_usv_series->setMarkerShape(QScatterSeries::MarkerShapeRectangle);
+    avg_usv_series->setColor(green);
+    avg_usv_series->attachAxis(axis_time);
+    avg_usv_series->attachAxis(axis_usv);
+
+    ui->graph->setRenderHint(QPainter::Antialiasing);
+    ui->graph->setChart(chart);
+}
+
+void MainWindow::graph_plot()
+{
+    QList<QString> split = serial_data.split(serial_separator.trimmed());
+    int time = split.at(0).toInt();
+    int cpm = split.at(1).toInt();
+    double avg_cpm = split.at(2).toDouble();
+    double usv = split.at(3).toDouble();
+    double avg_usv = split.at(4).toDouble();
+
+    cpm_series->append(time, cpm);
+    avg_cpm_series->append(time, avg_cpm);
+    usv_series->append(time, usv);
+    avg_usv_series->append(time, avg_usv);
+
+    if (cpm_series->count() > 10) {
+        cpm_series->removePoints(0, cpm_series->count() - 10);
+    } else {
+        axis_time->setTickCount(cpm_series->count());
+    }
+
+    int min_time = cpm_series->at(0).x();
+    int max_time = cpm_series->at(cpm_series->count() - 1).x();
+    axis_time->setRange(min_time, max_time);
+
+    int max_cpm = 0;
+    for (QPointF point : cpm_series->points()) {
+
+        if (point.y() > max_cpm) max_cpm = point.y();
+    }
+    axis_cpm->setRange(0, max_cpm + 10);
+
+    int max_usv = 0;
+    for (QPointF point : usv_series->points()) {
+        if (point.y() > max_usv) max_usv = point.y();
+    }
+    axis_usv->setRange(0, max_usv + 1);
+
+    ui->graph->repaint();
+    ui->button_save_graph->setEnabled(true);
+}
+
+/* ========== UI EVENTS ========= */
 
 void MainWindow::on_list_ports_itemSelectionChanged()
 {
@@ -200,7 +323,6 @@ void MainWindow::on_button_clear_formatted_clicked()
     ui->button_export->setEnabled(false);
 }
 
-
 void MainWindow::on_button_save_clicked()
 {
     QString filename = QFileDialog::getSaveFileName(this, "Save File", "", "Text Files (*.txt);;All Files (*)");
@@ -216,7 +338,6 @@ void MainWindow::on_button_save_clicked()
         QMessageBox::critical(this, "Error", "Unable to save file");
     }
 }
-
 
 void MainWindow::on_button_export_clicked()
 {
@@ -247,6 +368,32 @@ void MainWindow::on_button_export_clicked()
         QMessageBox::information(this, "Success", QString("File '%1' exported successfully").arg(filename));
     } else {
         QMessageBox::critical(this, "Error", "Unable to export file");
+    }
+}
+
+
+void MainWindow::on_button_clear_graph_clicked()
+{
+    cpm_series->clear();
+    avg_cpm_series->clear();
+    usv_series->clear();
+    avg_usv_series->clear();
+
+    ui->button_save_graph->setEnabled(false);
+}
+
+
+void MainWindow::on_button_save_graph_clicked()
+{
+    QPixmap pixmap = ui->graph->grab();
+
+    QString filename = QFileDialog::getSaveFileName(this, "Save Image", "", "PNG Files (*.png);;JPG Files (*.jpg);;All Files (*)");
+    if (filename.isEmpty()) return;
+
+    if (pixmap.save(filename)) {
+        QMessageBox::information(this, "Success", QString("Image '%1' saved successfully").arg(filename));
+    } else {
+        QMessageBox::critical(this, "Error", "Unable to save image");
     }
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QtSerialPort/qserialportinfo.h>
+#include <QtCharts>
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -41,6 +42,10 @@ private slots:
 
     void on_button_export_clicked();
 
+    void on_button_clear_graph_clicked();
+
+    void on_button_save_graph_clicked();
+
 private:
     Ui::MainWindow *ui;
     QSerialPort *port;
@@ -49,6 +54,18 @@ private:
     QString serial_data;
     QString serial_separator;
     QString serial_format;
+    QLineSeries *cpm_series;
+    QScatterSeries *avg_cpm_series;
+    QLineSeries *usv_series;
+    QScatterSeries *avg_usv_series;
+    QChart *chart;
+    QValueAxis *axis_time;
+    QValueAxis *axis_cpm;
+    QValueAxis *axis_usv;
+    QColor yellow;
+    QColor red;
+    QColor blue;
+    QColor green;
 
     void get_ports();
     void toggle_ui_elements();
@@ -57,5 +74,7 @@ private:
     void output_add_row();
     void output_make_readonly_centered(int row);
     bool output_valid_format();
+    void graph_setup();
+    void graph_plot();
 };
 #endif // MAINWINDOW_H
